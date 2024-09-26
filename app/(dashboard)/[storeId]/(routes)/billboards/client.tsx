dc6c07c5074d31a17b7e3e454d04eebd574ef708
ajay-mandal <ajayrox48@gmail.com>
2024-09-27 00:42:43 +0530

    billboard imgeupload done

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/client.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+
+
+const BillboardClient = () => {
+    const route = useRouter();
+    const params = useParams();
+
+    return ( 
+        <>
+            <div className="flex justify-between items-center">
+                <Heading 
+                title="Billboards (0)"
+                description="Manage billboards for your store"
+                />
+                <Button onClick={()=> route.push(`/${params.storeId}/billboards/new`)}>
+                    <Plus className="mr-2 h-4 w-4"/>
+                    Add New
+                </Button>
+            </div>
+            <Separator />
+        </>
+    );
+}
+ 
+export default BillboardClient;
