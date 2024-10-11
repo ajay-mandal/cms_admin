7ee16f964e12eb3fcb9b15e0dd3bfd9d4d4fdb7e
ajay-mandal <ajayrox48@gmail.com>
2024-10-12 00:32:02 +0530

    products api

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/product-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/product-form.tsx
@@ -130,7 +130,7 @@ export const ProductForm: React.FC<ProductProps> = ({
             route.refresh();
             toast.success("Product deleted.")
         }catch (error) {
-            toast.error("Make sure you removed all categories using this product first.");
+            toast.error("Something went wrong.");
         } finally {
             setLoading(false);
             setOpen(false);
---
+++ app/api/[storeId]/products/route.ts
@@ -0,0 +1,121 @@
+import { currentUserServerSide } from "@/hooks/currentUserServerSide";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function POST (
+    req: Request,
+    { params} : { params : { storeId: string}}
+) {
+    try{
+        const user = await currentUserServerSide();
+        if (!user) {
+            return new NextResponse("Unauthenticated", {status: 401});
+        }
+    
+        const body = await req.json();
+        const { name, price, categoryId, colorId, sizeId, images, isFeatured, isArchived } = body;
+
+        if(!name) {
+            return new NextResponse("Name is required", {status: 400});
+        }
+
+        if(!images || !images.length) {
+            return new NextResponse("Images are required", {status: 400});
+        }
+        if(!price) {
+            return new NextResponse("Price is required", {status: 400});
+        }
+        if(!categoryId) {
+            return new NextResponse("categoryId is required", {status: 400});
+        }
+        if(!colorId) {
+            return new NextResponse("colorId is required", {status: 400});
+        }
+        if(!sizeId) {
+            return new NextResponse("sizeId is required", {status: 400});
+        }
+
+        if(!params.storeId) {
+            return new NextResponse("StoreID is required", {status: 400});
+        }
+
+        const storeByUserId = await db.store.findFirst({
+            where: {
+                id: params.storeId,
+                userId: user.id
+            }
+        })
+
+        if(!storeByUserId) {
+            return new NextResponse("Unauthorized", {status: 403})
+        }
+    
+        const products = await db.product.create({
+            data: {
+                name, 
+                price, 
+                categoryId, 
+                colorId, 
+                sizeId, 
+                images: {
+                    createMany: {
+                        data: [
+                            ...images.map((image: { url: string}) => image)
+                        ]
+                    }
+                },
+                isFeatured, 
+                isArchived,
+                storeId: params.storeId
+            }
+        });
+
+        return NextResponse.json(products);
+    } catch (error) {
+        console.log('[PRODUCTS_POST]',error);
+        return new NextResponse("Internal error", {status: 500});
+    }
+}
+
+export async function GET (
+    req: Request,
+    { params} : { params : { storeId: string}}
+) {
+    try{
+
+        const { searchParams } = new URL(req.url);
+        const categoryId = searchParams.get("categoryId") || undefined;
+        const colorId = searchParams.get("colorId") || undefined;
+        const sizeId = searchParams.get("sizeId") || undefined;
+        const isFeatured = searchParams.get("isFeatured");
+
+        if(!params.storeId) {
+            return new NextResponse("StoreID is required", {status: 400});
+        }
+    
+        const products = await db.product.findMany({
+            where: {
+                storeId: params.storeId,
+                categoryId,
+                colorId,
+                sizeId,
+                isFeatured: isFeatured ? true : undefined,
+                isArchived: false
+            }, 
+            include: {
+                images: true,
+                category: true,
+                color: true,
+                size: true
+            },
+            orderBy: {
+                createdAt: 'desc'
+            }
+        });
+
+        return NextResponse.json(products);
+    } catch (error) {
+        console.log('[PRODUCTS_GET]',error);
+        return new NextResponse("Internal error", {status: 500});
+    }
+}
