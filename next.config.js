4e30ec5580c8152e03ff64a80cc6f2c4a91bac8f
ajay-mandal <ajayrox48@gmail.com>
2024-10-23 11:32:55 +0530

    stripe for payments

--- next.config.js
+++ next.config.js
@@ -8,6 +8,19 @@ const nextConfig = {
             port: '',
             }
         ]
+    },
+    async headers() {
+        return [
+            {
+                // matching all API path
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET, DELETE, POST, PUT, OPTIONS" },
+                    { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization"}
+                ]
+            }
+        ]
     }
 }
 
