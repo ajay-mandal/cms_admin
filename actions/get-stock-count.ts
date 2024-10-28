203af645c4587a780fa04581341d6fe0ec901c7f
ajay-mandal <ajayrox48@gmail.com>
2024-10-28 16:17:32 +0530

    stock count update

--- actions/get-stock-count.ts
+++ actions/get-stock-count.ts
@@ -1,12 +1,17 @@
 import { db } from "@/lib/db";
 
 export const getStockCount = async(storeId: string) => {
-    const stockCount = await db.product.count({
+    const products = await db.product.findMany({
         where: {
             storeId,
             isArchived: false
+        },
+        select: {
+            quantity: true
         }
     });
+
+    const stockCount = products.reduce((total, product) => total + product.quantity, 0);
     
     return stockCount;
 }
